daa2635aa4025471a913e2cdb79b53a31dd0f99c
krish <mlgman1o4@gmail.com>
2022-07-02 01:22:44 -0700

    basic help cmd

--- Cogs/utility.py
+++ Cogs/utility.py
@@ -6,12 +6,16 @@ class Utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True, brief='help command', aliases=['h'])
-    async def help(self, ctx):
-        embed = discord.Embed(title="HELP", description="thingy", color=0xff0000)
-        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-        embed.set_footer(text="regarewg")
-        await ctx.send(embed=embed)
+    @commands.command(pass_context = True, brief = 'All commands for bot', aliases = ['h'])
+    async def help(self,ctx):
+        embed = discord.Embed(
+            title = 'Help',
+            description = 'List of All Commands'
+        )
+        
+        embed.set_footer(text=f'Requested by - {ctx.author}', icon_url=ctx.author.avatar_url)
+        embed.add_field(name = 'Music CMDS', value = '`play`, `leave`, `pause`, `resume`, `queue`, `remove`, `skip`, `loopsong`, `loopqueue`, `unloop`, `shuffle`, `current`')
+        await ctx.send(embed = embed)
 
     
 def setup(bot):
