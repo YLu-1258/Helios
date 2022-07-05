c48379ecfece369f7f0b4fa7ed8f4a4543fea2d9
William Yuan <wi11iamyuan.1234@gmail.com>
2022-07-04 20:57:28 -0700

    deleted clear cuz we dont use it other than when destroying bot and destroying bot does that anyway

--- Cogs/music.py
+++ Cogs/music.py
@@ -124,16 +124,7 @@ class Queue:
                 print("REPMODE 2: ADDED TO POS")
             else:
                 self.pos = 0 #loop back to first song
-                print("REPMODE 2: LOOPBACK")
-
-
-    def clear(self):
-        '''
-        Clear Queue
-        '''
-        self._queue.clear()
-        self.pos = 0
-        self.repmode=0        
+                print("REPMODE 2: LOOPBACK")       
 
 class Player(commands.Cog):
     def __init__(self, ctx):
@@ -254,7 +245,6 @@ class Player(commands.Cog):
 
     def destroy(self, guild):
         """Disconnect and cleanup the player."""
-        self.Queue.clear()
         return self.bot.loop.create_task(self._cog.cleanup(guild))
 
     async def remove(self, pos):
