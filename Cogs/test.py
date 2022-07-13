12f654a468ce3b6e8845c71aafbc48d207347b6b
Yixuan-LULU <54480938+Yixuan-LULU@users.noreply.github.com>
2022-07-12 22:42:47 -0700

    embee

--- Cogs/test.py
+++ Cogs/test.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from discord.utils import get
 from discord_ui import UI, SelectMenu, SelectOption
 
-Music_CMDS = "**` md.join `**\nMakes the music bot join the caller's voice channel\n\nAlternatives: *` arm.joi, .j `*\n\n" + "**` md.leave `**\nMakes the music bot leave the caller's voice channel\n\nAlternatives: *` arm.l `*\n\n" + "**` md.play {song} `**\nPlays or queues requested song\n\n__Song Options__\n- Request a song by typing the name of it\n- Provide a Youtube url\n\nAlternatives: *` arm.p, .paly, .pl `*\n\n"
+Music_CMDS = "```yaml\n .join ```\nMakes the music bot join the caller's voice channel\n\nAlternatives: `.joi, .j`\n\n" + "```yaml\n .leave ```\nMakes the music bot leave the caller's voice channel\n\nAlternatives: `.l `\n\n" + "```yaml\n .play {song} ```\nPlays or queues requested song\n\n__Song Options__\n- Request a song by typing the name of it\n- Provide a Youtube url\n\nAlternatives: `.p, .paly, .pl`\n\n"
 
 
 class Test(commands.Cog):
