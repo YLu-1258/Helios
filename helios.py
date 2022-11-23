5bc54613fbd32251306bd3fe830300831b904a87
William Yuan <wi11iamyuan.1234@gmail.com>
2022-11-22 21:05:43 -0800

    discord

--- helios.py
+++ helios.py
@@ -2,22 +2,23 @@ import discord
 import options
 from discord.ext import commands
 from pathlib import Path
+import asyncio
 
 class Helios(commands.Bot):
     def __init__(self):
         super().__init__(intents=discord.Intents.default(), command_prefix=".", help_command=None)
         self._cogs = [path.stem for path in Path("./Cogs").glob("*.py")]
     
-    def load_cogs(self):    
+    async def load_cogs(self):    
         print("Loading cogs")
         for cog in self._cogs:
             if cog not in  ["options", "Utility", "pafy", "youtube_dl", "spotipy", "redis", "packaging", "lyricsgenius", "bs4", "nacl", "discord"]:
-                self.load_extension(f"Cogs.{cog}")
+                await self.load_extension(f"Cogs.{cog}")
                 print(f"{cog} commands have been loaded")
         print("All cogs Loaded")
 
-    def run(self):
-        self.load_cogs()
+    async def run(self):
+        await self.load_cogs()
         super().run(options.DISCORD_TOKEN, reconnect= True)
     
     async def on_ready(self):
